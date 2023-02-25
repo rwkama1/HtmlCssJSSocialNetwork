97226b992f1a250658dca7f23babdfda31fcaf40
rwkama1 <rwkamandriw@gmail.com>
2023-02-24 23:42:17 -0300

    restapiaddimage

--- REST_API/rest_api_images.js
+++ REST_API/rest_api_images.js
@@ -4,29 +4,43 @@ class APIRESTImages
  static RESTAPIURL ="https://api-next-social-network-private.vercel.app/api";
 
       
-       static addImage=async()=>
+       static addImage=async(imagedata)=>
        {
         
        
-             let URLALBUMIMAGE=this.RESTAPIURL+`/albumimage/getAlbumImagebyUser`;
+        const {idalbumphoto,title,description,urlimage,visibility} = imagedata;
+        
+
+        let POSTURLIMAGE=this.RESTAPIURL+"/images/image"
+
+        let headersList = {
+          "Accept": "*/*",
+          "Content-Type": "application/json"
+         }
+         
  
-             let headersList = {
-               "Accept": "*/*",
-              
-              }
-              
-              let response = await fetch(URLALBUMIMAGE, { 
-                method: "GET",
-                headers: headersList
-              });
-              
-            if(!response.ok)
-            {
-             const error=await response.text();
-             throw new Error(error);
-            }
-            let data = await response.json();
-            return data;
+         let bodyContent = JSON.stringify({
+          "idalbumphoto": idalbumphoto ,
+          "title": title,
+          "description":description ,
+          "urlimage":urlimage ,
+          "visibility":visibility 
+        
+        });
+ 
+         var requestOptions = {
+          method: "POST",
+          body: bodyContent,
+          headers: headersList
+         };
+ 
+      const response=await fetch(POSTURLIMAGE, requestOptions);
+       if(!response.ok)
+       {
+        const error=await response.text();
+        throw new Error(error);
+       }
+       return true;
        }  
  
   }     
